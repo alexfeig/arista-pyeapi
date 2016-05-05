97200ccd9479ec32a105cde0ac785bebfa359f60
Alex Feigenson <alex.feigenson@gmail.com>
2016-05-04 23:41:58 -0700

    First round before running out of steam. This will save a configuration, then back it up with a nice timestamped filename. Next step is to copy the .swi over.

--- veos-update/README.md
+++ veos-update/README.md
@@ -1,8 +1,9 @@
-This script will take an IP address, username, and password as arguments and use
- SCP to copy a veos .swi image to a veos virtual machine. It will then set the b
-oot image to the new .swi and reload it.
+This script will take an IP address and username as arguments and use SCP to copy a vEOS .swi image to a veos virtual machine. It will then set the boot image to the new .swi and reload it.
+
+Prior to updating, it will write the configuration.
 
 Todo:
 
 * Downgrade handling?
 * Release notes?
+* Diff config before saving?
---
+++ veos-update/veos-update.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+
+import getpass
+import pyeapi
+import argparse
+from paramiko import SSHClient
+from scp import SCPClient
+from datetime import datetime
+
+# Gathers command line arguments.
+parser = argparse.ArgumentParser(description='Upgrades a virtual machine running vEOS to another .swi version.')
+parser.add_argument('-u', '--user', type=str, help='vEOS Username - must have privilege 15', required=True)
+parser.add_argument('-i', '--ip', type=str, help='vEOS IP Address', required=True)
+# TODO: parser.add_argument('-s', '--swi', type=str, help='SWI Image', required=True)
+args = parser.parse_args()
+
+# Gathers password.
+password = getpass.getpass("vEOS Password: ")
+
+# Note - if you don't use return_node=True, enable is not available. See https://github.com/arista-eosplus/pyeapi/blob/develop/pyeapi/client.py#L386
+# Also see https://github.com/arista-eosplus/pyeapi/issues/37
+# Let's start by connecting to the vEOS VM
+node = pyeapi.connect(host=args.ip,password=password,return_node=True)
+
+# Copy running-config to startup-config
+node.enable('copy running-config startup-config')
+
+ssh = SSHClient()
+# If you don't do this below, it won't accept the host key.
+ssh.load_system_host_keys()
+# Connect to SSH
+ssh.connect(hostname=args.ip,username=args.user,password=password)
+# Paramiko - set transport to SSH
+scp = SCPClient(ssh.get_transport())
+
+# Backup Config
+# TODO: Add exception here to catch non priv15
+datetime = datetime.now().strftime("%Y%m%d-%H%M%S")
+scp.get('/mnt/flash/startup-config',args.ip + '-startup-config-' + datetime)
+
+scp.close()
