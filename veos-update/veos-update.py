739d0c37f0ba29f5c3bcfcbae02b8cc83a58fc62
Alex Feigenson <alex.feigenson@gmail.com>
2016-05-05 09:05:39 -0700

    Additional changes that didn't make last commit

--- veos-update/veos-update.py
+++ veos-update/veos-update.py
@@ -8,6 +8,7 @@ from scp import SCPClient
 from datetime import datetime
 
 # Gathers command line arguments.
+# TODO: Enable password
 parser = argparse.ArgumentParser(description='Upgrades a virtual machine running vEOS to another .swi version.')
 parser.add_argument('-u', '--user', type=str, help='vEOS Username - must have privilege 15', required=True)
 parser.add_argument('-i', '--ip', type=str, help='vEOS IP Address', required=True)
@@ -17,7 +18,8 @@ args = parser.parse_args()
 # Gathers password.
 password = getpass.getpass("vEOS Password: ")
 
-# Note - if you don't use return_node=True, enable is not available. See https://github.com/arista-eosplus/pyeapi/blob/develop/pyeapi/client.py#L386
+# Note - if you don't use return_node=True, enable is not available.
+# See https://github.com/arista-eosplus/pyeapi/blob/develop/pyeapi/client.py#L386
 # Also see https://github.com/arista-eosplus/pyeapi/issues/37
 # Let's start by connecting to the vEOS VM
 node = pyeapi.connect(host=args.ip,password=password,return_node=True)
@@ -40,6 +42,12 @@ datetime = datetime.now().strftime("%Y%m%d-%H%M%S")
 scp.get('/mnt/flash/startup-config',args.ip + '-startup-config-' + datetime)
 
 # Copy SWI Image
+# TODO: Add in some sort of feedback here so folks know it's copying the file. Also, add error handling
 scp.put(args.swi,remote_path='/mnt/flash/')
 
+node.config(('boot system flash:/'+args.swi))
+
 scp.close()
+
+# If force isn't used, API will not allow for a reload.
+node.enable('reload force')
